a0139c19379fe41cb4941470acf8af8b3e824bb9
anokhina <anokhina.veronica@gmail.com>
2016-12-02 12:58:12 +0300

    Back button used to go the prev dir. Buttons added.

--- src/ru/org/sevn/audiobookplayer/DirActivity.java
+++ src/ru/org/sevn/audiobookplayer/DirActivity.java
@@ -20,7 +20,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.EmptyStackException;
 import java.util.List;
+import java.util.Stack;
 
 import android.app.ListActivity;
 import android.content.Context;
@@ -31,6 +33,8 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -52,6 +56,9 @@ public class DirActivity extends ListActivity {
     
     private Bitmap houseBitmap;
     private Bitmap lastBitmap;
+    private Stack<String> folderNames;
+    private Stack<Integer> folderPos;
+    private boolean buttonInList;
 
     static class MyArrayAdapter extends ArrayAdapter<DirInfo> {
         private int itemLayout;
@@ -91,6 +98,8 @@ public class DirActivity extends ListActivity {
         houseBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.house);
         lastBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.recent);
         lastOpened = new ArrayList<>();
+        folderNames = new Stack<>();
+        folderPos = new Stack<>();
 
         File externalStore = Environment.getExternalStorageDirectory();
         Bundle extra = getIntent().getExtras();
@@ -104,12 +113,19 @@ public class DirActivity extends ListActivity {
             }
         }
         
+        currentDir = externalStore;
         rootDir = readRootDir();
-        setDataList(externalStore, rootDir);
         //android.R.layout.simple_list_item_1
         final ArrayAdapter<DirInfo> adapter = new MyArrayAdapter(this, R.layout.list_img_item, datalist);
         setListAdapter(adapter);
+        setContentView(R.layout.activity_dir);
     }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+        setDataList(currentDir, rootDir);
+    }    
     
     private File readRootDir() {
         SharedPreferences prefs = getSharedPreferences(MainActivity.PREF_NAME, MODE_PRIVATE);
@@ -118,8 +134,12 @@ public class DirActivity extends ListActivity {
     
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        DirInfo selectedValue = (DirInfo)getListAdapter().getItem(position);
+    	selectDir((DirInfo)getListAdapter().getItem(position));
+    }
+    
+    private void selectDir(DirInfo selectedValue) {
         if (selectedValue.getFile() == null) {
+        	saveListScroll();
         	setDataListLastBook(currentDir, rootDir);
             ((MyArrayAdapter)getListAdapter()).notifyDataSetChanged();
         } else
@@ -144,18 +164,37 @@ public class DirActivity extends ListActivity {
 	        	}
     		}
         } else {
-            setDataList(selectedValue.getFile(), rootDir);
-            ((MyArrayAdapter)getListAdapter()).notifyDataSetChanged();
+        	saveListScroll();
+        	updateDataList(selectedValue.getFile(), rootDir);
         }
     }
     
-    private boolean isNavigatedDir(DirInfo selectedValue) {
+    private void saveListScroll() {
+		this.folderPos.push(getListView().getLastVisiblePosition());
+	}
+    private int popListScroll() {
+    	Integer ret = null;
+    	try {
+    		ret = this.folderPos.pop();
+    	} catch (EmptyStackException e) {}
+    	if (ret != null) {
+    		return ret;
+    	}
+    	return 0;
+    }
+
+	private boolean isNavigatedDir(DirInfo selectedValue) {
         return FILE_UPDIR_LABEL.equals(selectedValue.getLabel()) || FILE_ROOTDIR_LABEL.equals(selectedValue.getLabel());
     }
     
     private Comparator<File> fileComparator = new FileNameComparator();
     
     private File currentDir;
+    
+    private void updateDataList(File fdir, File rootDir) {
+        setDataList(fdir, rootDir);
+        ((MyArrayAdapter)getListAdapter()).notifyDataSetChanged();
+    }
     private void setDataList(File fdir, File rootDir) {
     	currentDir = fdir;
         datalist.clear();
@@ -169,15 +208,20 @@ public class DirActivity extends ListActivity {
             if (fdirInfo != null && fdirInfo.getBookDir() != null) {
                 setDataList(parent.getParentFile(), rootDir);
             } else {
-                if (rootDir != null && rootDir.isDirectory() && rootDir.canRead()) {
-                    datalist.add(new DirInfo(rootDir, FILE_ROOTDIR_LABEL).setBitmap(houseBitmap));
-                }
-                if (parent != null && parent.canRead()) {
-                    datalist.add(new DirInfo(null, FILE_LAST_LABEL).setBitmap(lastBitmap));
-                }
-                if (parent != null && parent.canRead()) {
-                    datalist.add(new DirInfo(parent, FILE_UPDIR_LABEL));
-                }
+            	
+            	pushDir(fdir, false);
+            	
+            	if (buttonInList) {
+	                if (rootDir != null && rootDir.isDirectory() && rootDir.canRead()) {
+	                    datalist.add(new DirInfo(rootDir, FILE_ROOTDIR_LABEL).setBitmap(houseBitmap));
+	                }
+	                if (parent != null && parent.canRead()) {
+	                    datalist.add(new DirInfo(null, FILE_LAST_LABEL).setBitmap(lastBitmap));
+	                }
+	                if (parent != null && parent.canRead()) {
+	                    datalist.add(new DirInfo(parent, FILE_UPDIR_LABEL));
+	                }
+            	}
                 
                 File[] fileList = fdir.listFiles();
                 if (fileList != null) {
@@ -192,16 +236,31 @@ public class DirActivity extends ListActivity {
         }        
     }
     
+    private void pushDir(File fdir, boolean force) {
+    	String ldir = null;
+    	try {
+    		ldir = folderNames.peek();
+    	} catch (EmptyStackException e) {}
+    	if (ldir != null && !force && ldir.equals(fdir.getAbsolutePath()) ) {} else {
+    		folderNames.push(fdir.getAbsolutePath());
+    	}
+    }
+    
     private ArrayList<DirInfo> lastOpened;
     private void setDataListLastBook(File fdir, File rootDir) {
+    	pushDir(fdir, true);
+
     	Collection<BookInfo> books = getMp3Player().getAppSettings().getLastBooks().values();
         datalist.clear();
-        if (rootDir != null && rootDir.isDirectory() && rootDir.canRead()) {
-            datalist.add(new DirInfo(rootDir, FILE_ROOTDIR_LABEL).setBitmap(houseBitmap));
-        }
-        if (fdir != null && fdir.canRead() && fdir.isDirectory()) {
-            datalist.add(new DirInfo(fdir, FILE_UPDIR_LABEL));
-        }
+        
+    	if (buttonInList) {
+	        if (rootDir != null && rootDir.isDirectory() && rootDir.canRead()) {
+	            datalist.add(new DirInfo(rootDir, FILE_ROOTDIR_LABEL).setBitmap(houseBitmap));
+	        }
+	        if (fdir != null && fdir.canRead() && fdir.isDirectory()) {
+	            datalist.add(new DirInfo(fdir, FILE_UPDIR_LABEL));
+	        }
+    	}
         if (lastOpened.size() == 0) {
             for(BookInfo bi : books) {
             	lastOpened.add(0, bi.getDirInfo());
@@ -213,4 +272,112 @@ public class DirActivity extends ListActivity {
     private Mp3Player getMp3Player() {
         return AppService.mp3Player;
     }
+    
+    public static final String SAVED_STATE_KEY_ROOT_DIR = "RootDir";
+    public static final String SAVED_STATE_KEY_CUR_DIR = "CurDir";
+    public static final String SAVED_STATE_KEY_STACK_DIR = "StackDir";
+    public static final String SAVED_STATE_KEY_SCROLL = "ScrollDirPos";
+    
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(SAVED_STATE_KEY_ROOT_DIR, rootDir.getAbsolutePath());
+        outState.putString(SAVED_STATE_KEY_CUR_DIR, currentDir.getAbsolutePath());
+        outState.putStringArrayList(SAVED_STATE_KEY_STACK_DIR, new ArrayList<>(this.folderNames));
+    	outState.putIntegerArrayList(SAVED_STATE_KEY_SCROLL, new ArrayList<>(this.folderPos));
+        super.onSaveInstanceState(outState);
+    }
+    protected void onRestoreInstanceState(Bundle state) {
+    	String pathRoot = state.getString(SAVED_STATE_KEY_ROOT_DIR);
+    	String pathCur = state.getString(SAVED_STATE_KEY_CUR_DIR);
+    	ArrayList<String> fldrNames = state.getStringArrayList(SAVED_STATE_KEY_STACK_DIR);
+    	ArrayList<Integer> fldrPos = state.getIntegerArrayList(SAVED_STATE_KEY_SCROLL);
+    	if (pathRoot != null) {
+    		rootDir = new File(pathRoot);
+    	}
+    	if (pathCur != null) {
+    		currentDir = new File(pathCur);
+    	}
+    	if (fldrNames != null) {
+    		folderNames.clear();
+    		folderNames.addAll(fldrNames);
+    	}
+    	if (fldrPos != null) {
+    		folderPos.clear();
+    		folderPos.addAll(fldrPos);
+    	}
+    	super.onRestoreInstanceState(state);
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+        	if(pressBack()) {
+        		return true;
+        	}
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+    
+    private boolean pressBack() {
+    	String ldir = null;
+    	File fldir = null;
+    	int vpos = 0;
+    	try {
+    		folderNames.pop();
+    		while (fldir == null && folderNames.size() > 0) {
+    			ldir = folderNames.pop();
+    			vpos = popListScroll();
+    			if(ldir != null) {
+    				fldir = new File(ldir);
+    				if (!fldir.canRead()) {
+    					fldir = null;
+    				}
+    			}
+    		}
+    	} catch (EmptyStackException e) {}
+    	if (fldir != null) {
+    		updateDataList(fldir, rootDir);
+    		getListView().setSelection(vpos);
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public void onClickGoHome(View view) {
+    	switch (view.getId()) {
+		case R.id.btnCancel:
+	        setResult(RESULT_CANCELED, this.getIntent());
+	        finish();
+			break;
+		case R.id.btnBack:
+        	if (!pressBack()) {
+    	        setResult(RESULT_CANCELED, this.getIntent());
+    	        finish();
+        	}
+			break;
+		case R.id.btnPrev:
+			File parent = null;
+			if (currentDir != null) {
+				parent = currentDir.getParentFile();
+			}
+			if (parent != null) {
+				selectDir(new DirInfo(parent, FILE_UPDIR_LABEL));
+			} else {
+	        	if (!pressBack()) {
+	    	        setResult(RESULT_CANCELED, this.getIntent());
+	    	        finish();
+	        	}
+			}
+			break;
+		case R.id.btnLast:
+			selectDir(new DirInfo(null, FILE_LAST_LABEL));
+			break;
+		case R.id.btnHome:
+			selectDir(new DirInfo(rootDir, FILE_ROOTDIR_LABEL));
+			break;
+		default:
+			break;
+		}
+    }
+    
+    //TODO stack max size
 }
